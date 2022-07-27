6709b56e954312829e4584249f599c8e882201bc
Anuar <anuar12102001@gmail.com>
2022-07-28 01:49:53 +0600

    Initial Commit

---
+++ SberTaskInfrastructure/Configurations/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SberTaskDLA.Models;
+using SberTaskDLA;
+
+namespace SberTaskInfrastructure.Configurations
+{
+
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentityCore<ApplicationUser>(o => o.User.RequireUniqueEmail = true);
+
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
+            builder.AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+            
+        }
+    }
+}
---
+++ SberTaskInfrastructure/Controllers/RequestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SberTaskInfrastructure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        [Route("/testMethod")]
+        [HttpGet]
+        public String TestGetMethod()
+        {
+            return "somethnig";
+        }
+    }
+}
